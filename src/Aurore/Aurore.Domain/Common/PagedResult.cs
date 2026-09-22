8eb12da10839e81488d57fe81a3456307e553df7
Niyaa1786 <j4code1786@gmail.com>
2026-09-22 20:34:06 +0700

    feat: implement IBaseRepository.cs, DomainException.cs and PagedResult.cs

---
+++ src/Aurore/Aurore.Domain/Common/PagedResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aurore.Domain.Common
+{
+    public class PagedResult<T>
+    {
+        public IReadOnlyList<T> Items { get; set; } = [];
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public bool HasNextPage => Page < TotalPages;
+        public bool HasPreviousPage => Page > 1;
+    }
+}
---
+++ src/Aurore/Aurore.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aurore.Domain.Interfaces
+{
+    public interface IBaseRepository <T, TId> where T : class
+    {
+        Task<T?> GetByIdAsync(TId id, CancellationToken ct);
+
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
