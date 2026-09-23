0619dfedf4158409b914ed51cdf7794b4de08aaa
Niyaa1786 <j4code1786@gmail.com>
2026-09-23 11:12:29 +0700

    feat: implement IUnitOfWork

---
+++ src/Aurore/Aurore.Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Aurore.Application.Common.Interfaces;
+using Aurore.Infrastructure.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aurore.Infrastructure.Persistence.Repositories
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext context) => _context = context;
+
+
+        public Task<int> SaveChangesAsync(CancellationToken ct = default) => _context.SaveChangesAsync(ct);
+    }
+}
