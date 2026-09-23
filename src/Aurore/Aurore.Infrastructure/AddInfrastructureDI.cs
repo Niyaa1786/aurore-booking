e0d5f5c355c25014ae9ae96057ceb066919926c0
Niyaa1786 <j4code1786@gmail.com>
2026-09-23 11:13:12 +0700

    feat: implement Infrastructure layer

---
+++ src/Aurore/Aurore.Infrastructure/AddInfrastructureDI.cs
@@ -0,0 +1,25 @@
+﻿using Aurore.Application.Common.Interfaces;
+using Aurore.Infrastructure.Persistence.Data;
+using Aurore.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aurore.Infrastructure
+{
+    public static class AddInfrastructureDI
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Aurore/Aurore.Infrastructure/Persistence/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aurore.Infrastructure.Persistence.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+    }
+}
