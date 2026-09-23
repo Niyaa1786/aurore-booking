bad618711a236ddc136d913f162b03ee77a0a489
Niyaa1786 <j4code1786@gmail.com>
2026-09-23 10:58:35 +0700

    feat: implement Application layer

---
+++ src/Aurore/Aurore.Application/AddApplicationDI.cs
@@ -0,0 +1,24 @@
+﻿using Aurore.Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aurore.Application
+{
+    public static class AddApplicationDI
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining<IUnitOfWork>();
+
+            services.Scan(scan =>
+                scan.FromAssemblyOf<IUnitOfWork>().AddClasses(c => c.Where(c => c.Name.EndsWith("UseCase")), publicOnly: false)
+                .AsSelf()
+                .WithScopedLifetime());
+
+            return services;
+        }
+    }
+}
---
+++ src/Aurore/Aurore.Application/Common/Exceptions/AppValidationException.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aurore.Application.Common.Exceptions
+{
+    public class AppValidationException : FluentValidation.ValidationException
+    {
+        public AppValidationException(string propertyName, string errorMessage) : base(new[] { new  ValidationFailure(propertyName, errorMessage) }) { }
+    }
+}
